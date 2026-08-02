4e0de8c6f04dc312447f39d79d155fbfac4b03c8
Jackson Silva <jacksonsilva.jdi@gmail.com>
2026-08-02 17:16:24 -0300

    Corrige conexão com banco de dados

--- database.py
+++ database.py
@@ -1,12 +1,13 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 conexao = mysql.connector.connect(
-    host ="localhost",
-    user ="root",
-    password = "020305",
-    database = "estoque"
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
 )
-
 cursor = conexao.cursor()
-
-conexao.commit()
